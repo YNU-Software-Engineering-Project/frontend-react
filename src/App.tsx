f8f4d95368405d97b7512ee357fdea0f67405d1d
kmellon-kth <lappel1004@gmail.com>
2024-09-29 00:57:02 +0900

    footer, header 추가 및 main 브랜치 merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import DefaultPage from 'pages/DefaultPage';
-import TermsOfService from 'pages/TermsofService';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DefaultPage />} />
-      <Route path="/termsOfService" element={<TermsOfService />} />
     </Routes>
   );
 }
