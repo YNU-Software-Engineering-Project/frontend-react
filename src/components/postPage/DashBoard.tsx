78a18f82046a1d54d9278dce960189663c9bf3b6
kmellon-kth <lappel1004@gmail.com>
2024-10-01 13:47:54 +0900

    저장

--- src/components/postPage/DashBoard.tsx
+++ src/components/postPage/DashBoard.tsx
@@ -1,7 +1,31 @@
 import style from 'styles/PostPage/DashBoard.module.css';
+import { PieChart } from '@mui/x-charts/PieChart';
 
 const DashBoard = () => {
-  return <>상황판</>;
+  const valueFormatter = (item: { value: number }) => `${item.value}%`;
+  return (
+    <>
+      <div className={style.wrapper}>
+        {/* <PieChart
+          height={300}
+          series={[
+            {
+              data: [
+                {
+                  label: 'Windows',
+                  value: 72.72,
+                },
+              ],
+              innerRadius: 80,
+              arcLabel: params => params.label ?? '',
+              arcLabelMinAngle: 20,
+              valueFormatter,
+            },
+          ]}
+        /> */}
+      </div>
+    </>
+  );
 };
 
 export default DashBoard;
