c8e7185b6f9041ae6a3f6b901558640b349e6774
0andrich <rlrpdj@gmail.com>
2024-10-03 11:44:11 +0900

    make branch

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import Funding from './funding';
+// import Chart from './chart'
+// import Wishlist from './wishList'
+// import Alarm from 'alarm';
+// import Myfunding from './myfunding';
+// import { BrowserRouter } from 'react-router-dom';
+
+// const root = ReactDOM.createRoot(
+// 	document.getElementById('root') as HTMLElement,
+// );
+// root.render(
+// 	<BrowserRouter>
+// 		{/* <App /> */}
+// 		{/* <Funding /> */}
+// 		<Wishlist />
+// 		{/* <Chart /> */}
+// 		{/* <Alarm /> */}
+// 		{/* <Myfunding /> */}
+// 	</BrowserRouter>,
+// );
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import router from "./routes/root";
+import { RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement,
-);
-root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<RouterProvider router={router} />);
+}
+
