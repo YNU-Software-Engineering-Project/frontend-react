f81dad8bb1ba1049dff7731a93d261961b2ab1e3
0andrich <rlrpdj@gmail.com>
2024-10-03 13:33:02 +0900

    [Feat] login

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import router from "./routes/root";
-import { RouterProvider } from "react-router-dom";
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
-	<RouterProvider router={router} />
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 );
